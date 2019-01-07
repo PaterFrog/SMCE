64c39e1460475c6b0a8b230ea1644e50948f4cba
PaterFrog <pater.frog@gmail.com>
2019-01-07 06:00:17 +0100

    Init

---
+++ control.lua
@@ -0,0 +1,12 @@
+--control.lua
+
+script.on_event(defines.events.on_player_created, function(event)
+	local player = game.players[event.player_index]
+		local inventory =  player.get_main_inventory()
+		local quickbar = player.get_quickbar()
+			if inventory.get_item_count("car") == 0 and quickbar.get_item_count("car") == 0
+			then player.insert{name='car', count=1}
+			else player.print("Player already has an SMCE!")
+		end
+	end
+)
