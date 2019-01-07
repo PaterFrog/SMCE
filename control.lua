e062f93ab10657cebf05b0de9342061827708de4
PaterFrog <pater.frog@gmail.com>
2019-01-07 13:24:27 +0100

    fixed endless loop

--- control.lua
+++ control.lua
@@ -5,7 +5,6 @@ script.on_event(defines.events.on_player_created, function(event)
 			if inventory.get_item_count("car") == 0 and quickbar.get_item_count("car") == 0
 			then
 				local position = { } --create empty table
-						::rpt::
 						repeat
 						--Pick a random position in a square of radius 15 tiles
 						position.x = math.random() * 30 - 15 --math.random() gets a random number 0 to 1. multiply by 30 => 0 to 30. subtract 15 => -15 to 15
@@ -13,9 +12,7 @@ script.on_event(defines.events.on_player_created, function(event)
 						until 	position.x > 5 or position.x < -5 or position.y > 5 or position.y < -5
 								position.x = position.x + player.position.x
 								position.y = position.y + player.position.y
-						if game.surfaces[1].can_place_entity{name = "car", position = position, force = game.forces.player,} == false
-						then goto rpt
-						end
+				position = game.surfaces[1].find_non_colliding_position("car", position, 20, 1)
 				game.surfaces[1].create_entity{
 					name = "car",
 					position = position,
